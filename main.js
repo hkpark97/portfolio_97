a58e94439e1ed156da802fdec384f0813326c011
hkpark97 <77339221+hkpark97@users.noreply.github.com>
2021-01-20 10:38:57 -0500

    JS : home transparent

--- main.js
+++ main.js
@@ -34,6 +34,16 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
+//Welcome home slowly fade to transparent as the window scroll down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior : 'smooth'}); 
